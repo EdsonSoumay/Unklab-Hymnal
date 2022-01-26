5365f201a710a276f936e1ec171328afe1d7929c
dennis28billy <s11810068@student.unklab.ac.id>
2022-01-27 00:10:28 +0800

    add navigation from ListResponsiveReading page

--- src/pages/ListResponsiveReading/index.js
+++ src/pages/ListResponsiveReading/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native'
 import { IcBack } from '../../assets'
 
-const ListResponsiveReading = () => {
+const ListResponsiveReading = ({navigation}) => {
     return (
         <View style={styles.page}>
             <View style={styles.headerWrapper}>
-                <TouchableOpacity style={styles.backButtonContainer}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButtonContainer}>
                     <IcBack/>
                 </TouchableOpacity>
                 <View style={styles.headerTitleContainer}>
@@ -15,73 +15,73 @@ const ListResponsiveReading = () => {
             </View>
             <ScrollView style={styles.listWrapper}>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>The Ten Commandments / Sepuluh Hukum</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Exodus 20:1-17 / Keluaran 20:1-17</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Godly / Kesalehan Hidup</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Psalm 1:1-6 / Mazmur 1:1-6</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Prayer / Doa</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Matthew 6:5-15; 7:7-11 / Matius 6:5-15; 7:7-11</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Love / Kasih</Text>
                         <Text style={styles.responsiveReadingSubtitle}>1 Corinthians 13:1-13 / 1 Korintus 13:1-13</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>The Good Shepherd / Gembala yang Baik</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Psalm 23:1-16 / Mazmur 23:1-6</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Consecration / Pengabdian Diri</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Romans 12:1-3, 9-21/ Roma 12:1-3, 9-21</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>The Christian Life / Kehidupan Kristus</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Matthew 5:3-16 / Matius 5:3-16</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Repentance / Pertobatan</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Psalm 51:1-17 / Mazmur 51:1-17</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>The Sabbath / Hari Sabat</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Genesis 2:1-3; Isaiah 58:11-14 / Keluaran 2:1-3 ; Yesaya 58:11-14</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Christ’s Suffering and Death / Penderitaan dan Kematian Kristus</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Isaiah 53:1-12 / Yesaya 53:1-12</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Christ’s Second Coming / Kedatangan Yesus Kedua Kali</Text>
                         <Text style={styles.responsiveReadingSubtitle}>John 14:1-3; Matthew 24:42-51 / Yohanes 14:1-3; Matius 24:42-51</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.listContainer}>
-                    <TouchableOpacity style={styles.responsiveReadingContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResponsiveReading')} style={styles.responsiveReadingContainer}>
                         <Text style={styles.responsiveReadingTitle}>Call to Youth / Panggilan kepada Pemuda</Text>
                         <Text style={styles.responsiveReadingSubtitle}>Ecclesiates 12:1-7, 13, 14 / Pengkhotbah 12:1-7, 13, 14</Text>
                     </TouchableOpacity>
